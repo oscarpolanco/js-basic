00df86c6c32e0579b03d65ef953d0cd8207271c1
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-17 22:16:07 -0400

    Define variables

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+var name = 'Oscar';
+console.log(name);
+
+var lastName = 'Polanco';
+console.log(lastName);
+
+var age = 18;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
