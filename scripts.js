7b9bce38062cab5cc01ab17139d001f836c56326
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-20 15:55:56 -0400

    Functions statements and expresions

--- scripts.js
+++ scripts.js
@@ -1,29 +1,19 @@
-// functions
-function calculateAge (yearOfBirth) {
-    var age = 2018 - yearOfBirth;
-    return age;
+// functions statements and expresions
+// function statement
+function someFun(par) {
+    // code
 }
 
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1969);
-var ageMary = calculateAge(1948);
-console.log(ageJohn);
-console.log(ageMike);
-console.log(ageMary);
-
-function yearsUntilRetirement(name, year) {
-    var age = calculateAge(year);
-    var retirement = 65 - age;
-
-    if (retirement >= 0) {
-        console.log(name + ' retires in ' + retirement + ' years.');
-
-    } else {
-        console.log(name + ' is already retired.');
-    }
-
+// function expresion
+var someFun = function(par) {
+    //code
 }
 
-yearsUntilRetirement('John', 1990);
-yearsUntilRetirement('Mike', 1969);
-yearsUntilRetirement('Mary', 1948);
+// expresions
+3 + 4;
+var x = 3;
+
+// statement
+if (x === 5) {
+    // do something
+}
