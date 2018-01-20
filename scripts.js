2a817ee260f0ae1282abaf2de0481829bd50b409
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-20 16:17:16 -0400

    Objects

--- scripts.js
+++ scripts.js
@@ -1,20 +1,27 @@
-// arrays
-var names = ['John', 'Jane', 'Mark'];
-var years = new Array(1990, 1969, 1948);
-
-console.log(names[1]);
-names[1] = 'Ben';
-console.log(names[1]);
-
-var john = ['John', 'Smith', 1990, 'designer', false];
-john.push('blue');
-console.log(john);
-john.unshift('Mr.');
-console.log(john);
-john.pop();
-john.shift();
-console.log(john);
-
-if (john.indexOf('teacher') === -1) {
-    console.log('John is not a teacher.');
+// objects
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
 }
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john['job'] = 'programer';
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jame';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
