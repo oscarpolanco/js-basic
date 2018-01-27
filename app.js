c82a79f85bdfc9c9af047b166d39b0e0568584a4
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-27 15:56:30 -0400

    Handle events

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-// Modufy
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-// Read
-// var x = document.querySelector('#score-0').textContent;
-// console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//callback
+// document.querySelector('.btn-roll').addEventListener('click', btn);
+
+// Anonymous function
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+});
