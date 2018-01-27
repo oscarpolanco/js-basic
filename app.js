7da3414617e0e19a7f0a5483a2148fd4cd72aaf6
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-27 16:48:23 -0400

    Continue with the DOM manipulation and don't repeat yourself

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ roundScore = 0;
 activePlayer = 0;
 
 
-document.querySelector('.dice').style.display = 'none';
+hideDice();
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
@@ -27,15 +27,42 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    scores[activePlayer] += roundScore;
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+        hideDice();
+        toggleClass('.player-' + activePlayer + '-panel', 'active');
+        toggleClass('.player-' + activePlayer + '-panel', 'winner');
+    } else {
+        nextPlayer();
+    }
+});
+
+function toggleClass(selector,classToToggle) {
+    document.querySelector(selector).classList.toggle(classToToggle);
+}
+
+function hideDice() {
+    document.querySelector('.dice').style.display = 'none';
+}
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    toggleClass('.player-0-panel', 'active');
+    toggleClass('.player-1-panel', 'active');
+
+    hideDice();
+}
