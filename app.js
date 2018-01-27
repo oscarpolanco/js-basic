72f9ea14b257f52420debcde056f8cccc4865b59
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-27 17:11:55 -0400

    Add a init function

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-hideDice();
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 //callback
 // document.querySelector('.btn-roll').addEventListener('click', btn);
@@ -45,6 +36,28 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         nextPlayer();
     }
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0 , 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    hideDice();
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
 
 function toggleClass(selector,classToToggle) {
     document.querySelector(selector).classList.toggle(classToToggle);
