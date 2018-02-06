bd50f12f8ef6f77e3958a3910cae0b73316f4454
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-06 17:01:12 -0400

    Object.create

--- scripts.js
+++ scripts.js
@@ -1,30 +1,17 @@
-// Function constructor
-
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
+// Object.create
+var personProto = {
+    calculateAge: function() {
+        console.log(2018 - this.yearOfBirth);
+    }
 };
 
-var Person = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-};
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'Teacher';
 
-Person.prototype.calculeAge = function () {
-    console.log(2018 - this.yearOfBirth);
-};
-
-Person.prototype.lastName = 'Smith';
-
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-john.calculeAge();
-jane.calculeAge();
-mark.calculeAge();
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
