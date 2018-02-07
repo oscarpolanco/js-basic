285c6abe2a8c2cc8ae677b90de2b34cd73227ded
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-07 16:19:05 -0400

    Objects vs primative

--- scripts.js
+++ scripts.js
@@ -1,17 +1,33 @@
-// Object.create
-var personProto = {
-    calculateAge: function() {
-        console.log(2018 - this.yearOfBirth);
-    }
+// Primative vs Objects
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+var obj = {
+    name: 'John',
+    age: 26
+}
+
+var obj2 = obj;
+obj.age = 30;
+
+console.log(obj.age);
+console.log(obj2.age);
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
 };
 
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'Teacher';
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
 
-var jane = Object.create(personProto, {
-    name: { value: 'Jane' },
-    yearOfBirth: { value: 1969 },
-    job: { value: 'designer' }
-});
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
