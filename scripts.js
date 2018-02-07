ffd106b949d8bcf7fa7e5329af78b6eaf525136d
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-07 19:49:02 -0400

    closure

--- scripts.js
+++ scripts.js
@@ -1,20 +1,35 @@
-// Immediatly invoked function expresions
+// closures
 
-/*
-function game() {
-    var score = Math.random() * 10;
-    console.log(score >= 5);
+function retirement(retirementAge) {
+    var a = ' year left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
 }
-game();
-*/
 
-(function () {
-    var score = Math.random() * 10;
-    console.log(score >= 5);
-})();
+var retirementUS = retirement(66);
+var retirementGermany = retirement(55);
+var retirementIceland = retirement(67);
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+retirement(66)(1990);
 
-// console.log(score);
-(function (goodLuck) {
-    var score = Math.random() * 10;
-    console.log(score >= 5 - goodLuck);
-})(5);
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var john = interviewQuestion('designer');
+var jane = interviewQuestion('teacher');
+
+console.log(john('John'));
+console.log(jane('Jane'));
