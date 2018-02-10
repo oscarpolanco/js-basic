e0ed0d6736f82d763b70abd0cc6a19d25d4cc927
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-10 18:11:14 -0400

    Add event listeners for the button and the enter key

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
+// BUDGET CONTROLLER
 var bugetController = (function() {
-    var x = 23;
 
-    var add = function (a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    };
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
     // Some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(bugetCtrl, UICtrl) {
-    var z = bugetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
+
+    var ctrlAddItem = function() {
+        // 1. Get filed input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add item ti the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the bufget on the UI
+        console.log('test');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
         }
-    };
+    });
 })(bugetController, UIController);
