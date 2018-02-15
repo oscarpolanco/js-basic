02d02a14aff6cd0ad6af7d0649bc471c378d6c1b
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-15 11:49:51 -0400

    Update the percentages of the items on the UI

--- app.js
+++ app.js
@@ -152,7 +152,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
 
     return {
@@ -214,6 +215,25 @@ var UIController = (function() {
 
         },
 
+        displayPercentages: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMString.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index) {
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+
         getDOMStrings: function() {
             return DOMString;
         }
@@ -253,10 +273,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         //  1. calculate the percentages
         budgetCtrl.calculatePerceteges();
+
         // 2. Read the percentages from the budget controller
         var percentages = budgetCtrl.getPercentage();
+
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
+
     };
 
     var ctrlAddItem = function() {
