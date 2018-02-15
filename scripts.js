22a9c04efc37babf9ccabbbbded31d191371e061
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-15 17:18:22 -0400

    Blocks and IIFEs

--- scripts.js
+++ scripts.js
@@ -1,50 +1,17 @@
-// Lecture: let and const
+// Lecture: Block and IIFEs
 
-// ES5
-var name5 = 'James Smith';
-var age5 = 23;
-name5 = 'James Miller';
-console.log(name5);
-
-// ES6
-const name6 = 'James Smith';
-let age6 = 23;
-// Error
-// name6 = 'James Miller';
-// console.log(name6);
-
-// ES5
-function driverLicense(passedTest) {
-
-    if (passedTest) {
-        console.log(firstName);
-        var firstName = 'John';
-        var yearOfBirth = 1990;
-    }
-    console.log(firstName + 'born in ' + yearOfBirth + ' is now officially allow to drive a car');
-};
-
-driverLicense(true);
-
-// ES6
-// let and const are block scope not function scope like var
-function driverLicense6(passedTest) {
-    // Error
-    // console.log(firstName);
-    let firstName;
-    const yearOfBirth = 1990;
-
-    if (passedTest) {
-        firstName = 'John';
-    }
-    console.log(firstName + 'born in ' + yearOfBirth + ' is now officially allow to drive a car');
-};
-
-driverLicense6(true);
-
-let i = 23;
-for (let i = 0; i < 5; i++) {
-    console.log(i);
+{
+    const a = 2;
+    let b = 3;
+    var c = 4;
 }
+console.log(c);
+// error
+// console.log(a + b);
 
-console.log(i);
+// ES5
+(function() {
+    var c = 3;
+})();
+// error
+// console.log(c);
