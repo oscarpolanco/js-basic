1cafbe57d4ad02557cc584c8948359fca92b7f29
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-16 11:25:28 -0400

    Lexical this on arrow functions

--- scripts.js
+++ scripts.js
@@ -1,23 +1,68 @@
-// Arrow functions
-
-const years = [1990, 1965, 1982, 1937];
+// Arrow functions 2
 
 // ES5
-var ages5 = years.map(function(el) {
-    return 2018 - el;
-});
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
 
-console.log(ages5);
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This box number ' + self.position + ' ahd it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+
+// box5.clickMe();
 
 // ES6
-let ages6 = years.map(el => 2018 - el);
-console.log(ages6);
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This box number ' + this.position + ' ahd it is ' + this.color;
+            alert(str);
+        });
+    }
+}
 
-ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}.`);
-console.log(ages6);
+// box6.clickMe();
 
-ages6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    const age = now - el;
-    return `Age element ${index + 1}: ${age}.`
-});
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This box number ' + this.position + ' ahd it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+
+box66.clickMe();
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function(friends) {
+    var arr = friends.map(function(el) {
+        return this.name + ' is firends with ' + el;
+    }.bind(this));
+    console.log(arr);
+}
+
+var friends = ['Bod', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+// ES6
+Person.prototype.myFriends6 = function(friends) {
+    const arr = friends.map(el => `${this.name} is firends with ${el}`);
+    console.log(arr);
+}
+
+var friends = ['Bod', 'Jane', 'Mark'];
+new Person('John').myFriends6(friends);
