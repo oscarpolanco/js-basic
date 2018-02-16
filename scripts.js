cadcabcc54d1cc105206e9c5e608623e5c42a8eb
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-16 14:47:51 -0400

    Arrays on ES6

--- scripts.js
+++ scripts.js
@@ -1,33 +1,51 @@
-// Destructuring
+// Arrays
+
+const boxes = document.querySelectorAll('.box');
 
 // ES5
-var john = ['John', 26];
-// var name = john[0];
-// var age = john[1];
+/*
+var boxesArr5 = Array.prototype.slice.call(boxes);
+
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+});
+*/
 
 // ES6
-const [name, age] = ['John', 26];
-console.log(name);
-console.log(age);
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
-const obj = {
-    firstName: 'John',
-    lastName: 'Smith'
-};
+// ES5
+/*
+for (var i = 0; i < boxesArr5.length; i++) {
+    if (boxesArr5[i].className === 'box blue') {
+        continue;
+    }
 
-const {firstName, lastName} = obj;
-console.log(firstName);
-console.log(lastName);
+    boxesArr5[i].textContent = 'I change to blue';
+}
+*/
 
-const {firstName: a, lastName: b} = obj;
-console.log(a);
-console.log(b);
+// ES6
+for (const cur of boxesArr6) {
+    if (cur.className.includes('blue')) {
+        continue;
+    }
 
-function calcAgeRetiretment(year) {
-    const age = new Date().getFullYear() - year;
-    return [age, 65 - age];
+    cur.textContent = 'I change to blue';
 }
 
-const [age2, retiretment] = calcAgeRetiretment(1990);
-console.log(age2);
-console.log(retiretment);
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur) {
+    return cur >= 18;
+});
+
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
