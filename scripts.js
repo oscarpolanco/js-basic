831b31a2f4acbb61fcd264271894bcbb0a6e1b63
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-02-16 16:25:58 -0400

    REST parameters

--- scripts.js
+++ scripts.js
@@ -1,27 +1,38 @@
-// Spread operator
-
-function addFourAges(a, b, c, d) {
-    return a + b + c + d;
-}
-
-var sum1 = addFourAges(18, 30, 12, 21);
-console.log(sum1);
+// Rest parameters
 
 // ES5
-var ages = [18, 30, 12, 21];
-var sum2 = addFourAges.apply(null, ages);
-console.log(sum2);
+/*
+function isFullAge5() {
+    // console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+    argsArr.forEach(function(cur) {
+        console.log((2018 - cur) >= 18);
+    });
+}
+
+isFullAge5(1990, 2005, 1965);
+isFullAge5(1990, 2005, 1965, 2016, 1987);
 
 // ES6
-const sum3 = addFourAges(...ages);
-console.log(sum3);
+function isFullAge6(...years) {
+    years.forEach(cur => console.log((2018 - cur) >= 18));
+}
+isFullAge6(1990, 2005, 1965);
+*/
 
-const familySmith = ['John', 'Jane', 'Mark'];
-const familyMiller = ['Mary', 'Bob', 'Ann'];
-const bigFamily = [...familySmith, 'Lily', ...familyMiller];
-console.log(bigFamily);
+function isFullAge5(limit) {
+    // console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+    argsArr.forEach(function(cur) {
+        console.log((2018 - cur) >= limit);
+    });
+}
 
-const h = document.querySelector('h1');
-const boxes = document.querySelectorAll('.box');
-const all = [h, ...boxes];
-Array.from(all).forEach(cur => cur.style.color = 'purple');
+isFullAge5(21, 1990, 2005, 1965);
+isFullAge5(1990, 2005, 1965, 2016, 1987);
+
+// ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2018 - cur) >= limit));
+}
+isFullAge6(16, 1990, 2005, 1965);
