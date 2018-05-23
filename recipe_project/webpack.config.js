5b2705b1e446831ba4000bb382cc34025be1bd46
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-05-22 20:03:25 -0400

    Add webpack dev server

--- recipe_project/webpack.config.js
+++ recipe_project/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPuglin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
   },
   // mode: 'development'
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPuglin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 };
