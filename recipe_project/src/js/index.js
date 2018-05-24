d3d180469c5b6e34c38d30a61563a246f30df58a
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-05-24 18:57:47 -0400

    Export and import files

--- recipe_project/src/js/index.js
+++ recipe_project/src/js/index.js
@@ -1,4 +1,6 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module!!!!!! Varibale x is ${x}`);
+import string from './models/Search';
+// import {add as a, multiply as b, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+// console.log(`Using imported functions! ${a(ID, 2)} and ${b(3, 5)}. ${string}`);
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${string}`);
