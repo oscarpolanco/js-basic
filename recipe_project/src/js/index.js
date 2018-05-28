196981a2e3ce683e7ec328ee9661e9885913dc64
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-05-28 18:37:03 -0400

    Add the search functionallity to the search model

--- recipe_project/src/js/index.js
+++ recipe_project/src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios'
+import Search from './models/Search';
 
-async function getResult(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '9b263d172248097f7ec9f2dd3066d6db';
-  try {
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-getResult('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResult();
--- recipe_project/src/js/models/Search.js
+++ recipe_project/src/js/models/Search.js
@@ -1,1 +1,19 @@
-export default 'I am a exported string.';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResult() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '9b263d172248097f7ec9f2dd3066d6db';
+    try {
+      const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
