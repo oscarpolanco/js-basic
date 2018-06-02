5a1b2036845f26b08d9aa6ed4f5d56847cd50f8e
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-02 14:28:35 -0400

    Add a config file to the keys, the recipe controller and make a example request of a single recipe

--- recipe_project/src/js/index.js
+++ recipe_project/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -10,6 +11,10 @@ import { elements, renderLoader, clearLoader } from './views/base';
 */
 const state = {};
 
+
+/**
+* SEARCH CONTROLLER
+*/
 const controlSearch = async () => {
   // 1) Get query form the view
   const query = searchView.getInput();
@@ -47,3 +52,10 @@ elements.searchResPages.addEventListener('click', e => {
   }
 
 });
+
+/**
+* RECIPE CONTROLLER
+*/
+const r = new Recipe(47746);
+r.getRecipe();
+console.log(r);
--- recipe_project/src/js/models/Search.js
+++ recipe_project/src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key, proxy } from '../config';
 
 export default class Search {
   constructor(query) {
@@ -6,8 +7,6 @@ export default class Search {
   }
 
   async getResult() {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '9b263d172248097f7ec9f2dd3066d6db';
     try {
       const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
       this.result = res.data.recipes;
