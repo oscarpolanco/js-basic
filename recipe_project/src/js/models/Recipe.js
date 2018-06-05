a75f5976b154b1b81cdcf1d78632a5b1b192e585
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-05 18:46:05 -0400

    Add the recipe view and upadate the recipe controller

--- recipe_project/src/js/models/Recipe.js
+++ recipe_project/src/js/models/Recipe.js
@@ -33,6 +33,7 @@ export default class Recipe {
   parseIngridients() {
     const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
     const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+    const units = [...unitsShort, 'kg', 'g'];
 
     const newIngridients = this.ingredients.map(el => {
       // 1) Uniform units
@@ -46,7 +47,7 @@ export default class Recipe {
 
       // 3) Parse ingridients into count, unit and ingridient
       const arrIng = ingredient.split(' ');
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
