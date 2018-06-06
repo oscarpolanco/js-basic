a271207caec050d20dcd33e3fd6e7cd9a5756b63
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-06 19:11:22 -0400

    Add the increase and decrease functionallity for the serving and ingridients using matches

--- recipe_project/src/js/views/recipeView.js
+++ recipe_project/src/js/views/recipeView.js
@@ -57,16 +57,16 @@ export const renderRecipe = recipe => {
             <svg class="recipe__info-icon">
                 <use href="img/icons.svg#icon-man"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.serving}</span>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text"> servings</span>
 
             <div class="recipe__info-buttons">
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-decrease">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-minus"></use>
                     </svg>
                 </button>
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-increase">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-plus"></use>
                     </svg>
@@ -110,5 +110,15 @@ export const renderRecipe = recipe => {
     </div>
   `;
   elements.recipe.insertAdjacentHTML('afterbegin', markup);
-
 };
+
+export const updateServingsIngredients = recipe => {
+  // Update servings
+  document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+  // Update Ingredients
+  const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+  countElements.forEach((el, i) => {
+    el.textContent = formatCount(recipe.ingredients[i].count);
+  });
+}
