342337d7233ad8bc73a2fed471a1357f725962c7
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-09 15:44:31 -0400

    Add all shopping list functionallity

--- recipe_project/src/js/models/List.js
+++ recipe_project/src/js/models/List.js
@@ -17,7 +17,6 @@ export default class List {
 
   deleteItem(id) {
     const index = this.items.findIndex(el => el.id === id);
-
     // [2, 4, 8] splice(1, 1) -> return 4, original array is [2, 8]
     // [2, 4, 8] slice(1, 1) -> return 4, original array is [2, 4, 8]
     this.items.splice(index, 1);
--- recipe_project/src/js/views/listView.js
+++ recipe_project/src/js/views/listView.js
@@ -4,7 +4,7 @@ export const renderItem = item => {
   const markup = `
     <li class="shopping__item" data-itemid=${item.id}>
         <div class="shopping__count">
-            <input type="number" value="${item.count}" step="${item.count}" class="shopping__cpunt-value">
+            <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
             <p>${item.unit}</p>
         </div>
         <p class="shopping__description">${item.ingredient}</p>
@@ -19,6 +19,7 @@ export const renderItem = item => {
 };
 
 export const deleteItem = id => {
-  const item = document.querySelector(`[data-itemid="[${id]}"]`);
-  item.parentElement.removeChild(item);
+  const item = document.querySelector(`[data-itemid="${id}"]`);
+  console.log(item);
+  if (item) item.parentElement.removeChild(item);
 };
